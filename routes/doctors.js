b8bd5b3a32e474acaa9e5062a163a137ada57d85
ATOROYO <davidatoroyosika@gmail.com>
2024-10-12 10:07:00 +0300

    Added doctors routes

--- routes/doctors.js
+++ routes/doctors.js
@@ -2,3 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const doctorController = require('../controllers/doctorController');
+
+// Doctor Routes
+router.get('/', doctorController.getAllDoctors);
+router.post('/add', doctorController.addDoctor);
+router.get('/:id', doctorController.getDoctorById);
+
+module.exports = router;
