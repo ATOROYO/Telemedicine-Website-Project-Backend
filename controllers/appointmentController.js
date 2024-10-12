42d8cc313776317ce25efb70dad530c00ffb0740
ATOROYO <davidatoroyosika@gmail.com>
2024-10-12 10:36:45 +0300

    Wrote code to books apointments

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -8,3 +8,16 @@ exports.getAllAppointments = (req, res) => {
     res.json(results);
   });
 };
+
+// Booking appointments
+exports.bookAppointment = (req, res) => {
+  const { patient_id, doctor_id, appointment_date } = req.body;
+  db.query(
+    'INSERT INTO appointments SET ?',
+    { patient_id, doctor_id, appointment_date, status: 'pending' },
+    (err, result) => {
+      if (err) throw err;
+      res.send('Appointment booked');
+    }
+  );
+};
