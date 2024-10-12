8cca0da4b3a413a5010ba6768e1ac0eb02ad686c
ATOROYO <davidatoroyosika@gmail.com>
2024-10-12 10:30:33 +0300

    Wrote code to add a doctor to the datbase

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -8,3 +8,16 @@ exports.getAllDoctors = (req, res) => {
     res.json(results);
   });
 };
+
+// Adding a doctor to the database
+exports.addDoctor = (req, res) => {
+  const { first_name, last_name, email, password, specialty } = req.body;
+  db.query(
+    'INSERT INTO users SET ?',
+    { first_name, last_name, email, password, role: 'doctor', specialty },
+    (err, result) => {
+      if (err) throw err;
+      res.send('Doctor added');
+    }
+  );
+};
