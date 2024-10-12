8ff1fb3112db27113335bd40633911a26cd65eb2
ATOROYO <davidatoroyosika@gmail.com>
2024-10-12 10:50:19 +0300

    Added more routes to the main file

--- index.js
+++ index.js
@@ -16,3 +16,13 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.use('/patients', patientRoutes);
+app.use('/doctors', doctorRoutes);
+app.use('/appointments', appointmentRoutes);
+app.use('/admin', adminRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Rhema Hospital Telemedicine Service');
+});
