5164d224916913250d50c317f541b7c204fa0a96
ATOROYO <davidatoroyosika@gmail.com>
2024-10-12 10:01:03 +0300

    Added Patients Routes to patients.js

--- routes/patients.js
+++ routes/patients.js
@@ -2,3 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const patientController = require('../controllers/patientController');
+
+// Patient Routes
+router.get('/', patientController.getAllPatients);
+router.post('/add', patientController.addPatient);
+router.get('/:id', patientController.getPatientById);
+
+module.exports = router;
