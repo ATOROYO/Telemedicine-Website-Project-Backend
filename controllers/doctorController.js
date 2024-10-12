73420f7facd65785b52edfd715ef4f7789cf3596
ATOROYO <davidatoroyosika@gmail.com>
2024-10-12 10:28:51 +0300

    Wrote codes to get all doctors fromthe database

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,2 +1,10 @@
 // Importing modules
 const db = require('../config/db');
+
+// Getting all doctors fromthe database
+exports.getAllDoctors = (req, res) => {
+  db.query('SELECT * FROM users WHERE role = "doctor"', (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+};
