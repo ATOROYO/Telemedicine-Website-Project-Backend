2414764c8ac8addf042c94600dfe4fe7b458ffc7
ATOROYO <davidatoroyosika@gmail.com>
2024-10-12 10:11:14 +0300

    Created appointments routes

--- routes/appointments.js
+++ routes/appointments.js
@@ -2,3 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const appointmentController = require('../controllers/appointmentController');
+
+// Appointment Routes
+router.get('/', appointmentController.getAllAppointments);
+router.post('/book', appointmentController.bookAppointment);
+router.get('/:id', appointmentController.getAppointmentById);
+
+module.exports = router;
