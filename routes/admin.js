202b378be446dba8b387f4e1c7bfdec8d70da041
ATOROYO <davidatoroyosika@gmail.com>
2024-10-12 10:15:48 +0300

    Added Admin Routes

--- routes/admin.js
+++ routes/admin.js
@@ -2,3 +2,9 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
+
+// Admin Routes
+router.get('/', adminController.getAdminDashboard);
+router.post('/addDoctor', adminController.addDoctor);
+
+module.exports = router;
