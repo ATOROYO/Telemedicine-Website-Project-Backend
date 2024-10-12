3c15ebe90f382927c298ec9b2da3842f44c5d532
ATOROYO <davidatoroyosika@gmail.com>
2024-10-12 10:49:02 +0300

    Added routes to the main file

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const db = require('./config/db');
+
+// Routes
+const patientRoutes = require('./routes/patients');
+const doctorRoutes = require('./routes/doctors');
+const appointmentRoutes = require('./routes/appointments');
+const adminRoutes = require('./routes/admin');
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
