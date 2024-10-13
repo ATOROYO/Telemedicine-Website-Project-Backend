ee0c30a527a3105ccf961f8c344204673939f578
ATOROYO <davidatoroyosika@gmail.com>
2024-10-13 18:36:40 +0300

    Added a function to add new doctor in admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,20 @@ const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
 
+// Add new doctor
+router.post('/doctors', ensureAdmin, (req, res) => {
+  const { first_name, last_name, email, specialization, schedule } = req.body;
+
+  db.query(
+    'INSERT INTO doctors (first_name, last_name, email, specialization, schedule) VALUES (?, ?, ?, ?, ?)',
+    [first_name, last_name, email, specialization, schedule],
+    (err, result) => {
+      if (err) throw err;
+      res.json({ message: 'Doctor added successfully' });
+    }
+  );
+});
+
 // Admin Routes
 router.get('/', adminController.getAdminDashboard);
 router.post('/addDoctor', adminController.addDoctor);
