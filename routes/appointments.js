714a89c3f94bbd4172368f91153460c724fb6782
ATOROYO <davidatoroyosika@gmail.com>
2024-10-13 18:51:55 +0300

    Wrote function to book an appointment in appointment.js

--- routes/appointments.js
+++ routes/appointments.js
@@ -4,6 +4,21 @@ const router = express.Router();
 const appointmentController = require('../controllers/appointmentController');
 const { ensureAuthenticated } = require('../middlewares/auth');
 
+// Book an appointment
+router.post('/', ensureAuthenticated, (req, res) => {
+  const { doctorId, appointmentDate, time } = req.body;
+  const patientId = req.session.patient.id;
+
+  db.query(
+    'INSERT INTO appointments (patient_id, doctor_id, appointment_date, time) VALUES (?, ?, ?, ?)',
+    [patientId, doctorId, appointmentDate, time],
+    (err, result) => {
+      if (err) throw err;
+      res.json({ message: 'Appointment booked successfully' });
+    }
+  );
+});
+
 // Appointment Routes (Protected)
 router.get('/', ensureAuthenticated, appointmentController.getAllAppointments);
 router.post(
