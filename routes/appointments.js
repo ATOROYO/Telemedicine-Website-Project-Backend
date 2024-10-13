c97f34a4490cb6b04bedfeaa26fe2e42b11c54d3
ATOROYO <davidatoroyosika@gmail.com>
2024-10-13 17:07:58 +0300

    Wrote code to protect the appointments routes

--- routes/appointments.js
+++ routes/appointments.js
@@ -4,9 +4,17 @@ const router = express.Router();
 const appointmentController = require('../controllers/appointmentController');
 const { ensureAuthenticated } = require('../middlewares/auth');
 
-// Appointment Routes
-router.get('/', appointmentController.getAllAppointments);
-router.post('/book', appointmentController.bookAppointment);
-router.get('/:id', appointmentController.getAppointmentById);
+// Appointment Routes (Protected)
+router.get('/', ensureAuthenticated, appointmentController.getAllAppointments);
+router.post(
+  '/book',
+  ensureAuthenticated,
+  appointmentController.bookAppointment
+);
+router.get(
+  '/:id',
+  ensureAuthenticated,
+  appointmentController.getAppointmentById
+);
 
 module.exports = router;
