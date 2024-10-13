bf7e7e0f0735c9329c4d6cdede1b2119387294e7
ATOROYO <davidatoroyosika@gmail.com>
2024-10-13 16:37:58 +0300

    Wrote code for registering new user

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,5 +1,43 @@
 // Importing modules
 const db = require('../config/db');
+const bcrypt = require('bcrypt');
+
+// Register a new patient
+exports.registerPatient = async (req, res) => {
+  const { first_name, last_name, email, password } = req.body;
+
+  // Check if the email is already registered
+  db.query(
+    'SELECT * FROM users WHERE email = ? AND role = "patient"',
+    [email],
+    async (err, results) => {
+      if (err) throw err;
+
+      if (results.length > 0) {
+        return res.status(400).json({ message: 'Email is already registered' });
+      } else {
+        // Hash the password before storing it
+        const hashedPassword = await bcrypt.hash(password, 10);
+        db.query(
+          'INSERT INTO users SET ?',
+          {
+            first_name,
+            last_name,
+            email,
+            password: hashedPassword,
+            role: 'patient',
+          },
+          (err, result) => {
+            if (err) throw err;
+            res
+              .status(201)
+              .json({ message: 'Patient registered successfully' });
+          }
+        );
+      }
+    }
+  );
+};
 
 // Getting all the patients
 exports.getAllPatients = (req, res) => {
