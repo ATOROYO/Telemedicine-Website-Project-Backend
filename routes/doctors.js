8b0fecadd1d81150d6f1d9da9d8850bde32dd709
ATOROYO <davidatoroyosika@gmail.com>
2024-10-13 18:40:34 +0300

    Wrote code to get list of doctors in doctors.js

--- routes/doctors.js
+++ routes/doctors.js
@@ -8,4 +8,12 @@ router.get('/', doctorController.getAllDoctors);
 router.post('/add', doctorController.addDoctor);
 router.get('/:id', doctorController.getDoctorById);
 
+// Get list of doctors
+router.get('/', (req, res) => {
+  db.query('SELECT * FROM doctors', (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
+
 module.exports = router;
