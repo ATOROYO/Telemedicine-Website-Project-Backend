446bafc3b0d169e18bb50d3fd407d07425655b02
ATOROYO <davidatoroyosika@gmail.com>
2024-10-16 22:07:16 +0300

    Changed a code in patient controller

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -130,7 +130,7 @@ exports.logout = (req, res) => {
 
 // Getting all the patients
 exports.getAllPatients = (req, res) => {
-  db.execute('SELECT * FROM users WHERE role = "patient"', (err, results) => {
+  db.query('SELECT * FROM users WHERE role = "patient"', (err, results) => {
     if (err) throw err;
     res.json(results);
   });
