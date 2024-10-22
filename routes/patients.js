67f1263e1bd9281f50db0ff0e794473a80b3fe2e
ATOROYO <davidatoroyosika@gmail.com>
2024-10-22 19:01:06 +0300

    Updated the registration route

--- routes/patients.js
+++ routes/patients.js
@@ -22,45 +22,7 @@ router.post('/add', patientController.addPatient);
 router.get('/:id', patientController.getPatientById);
 
 // Registration Route
-router.post('/register', async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-
-    // Check the incoming data
-    console.log(req.body);
-
-    if (!name || !email || !password) {
-      return res
-        .status(400)
-        .json({ success: false, message: 'All fields are required' });
-    }
-
-    const [existingUser] = await db.execute(
-      'SELECT * FROM patients WHERE email = ?',
-      [email]
-    );
-    if (existingUser.length > 0) {
-      return res
-        .status(400)
-        .json({ success: false, message: 'Email already exists' });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const [result] = await db.execute(
-      'INSERT INTO patients (name, email, password) VALUES (?, ?, ?)',
-      [name, email, hashedPassword]
-    );
-
-    console.log('Insert result: ', result); // Log the result of the insert query
-    return res
-      .status(201)
-      .json({ success: true, message: 'Registration successful' });
-  } catch (error) {
-    console.error('Error during registration:', error);
-    return res.status(500).json({ success: false, message: 'Server error' });
-  }
-});
+router.post('/register', patientController.registerPatient);
 
 // Login route
 router.post('/login', patientController.loginPatient);
