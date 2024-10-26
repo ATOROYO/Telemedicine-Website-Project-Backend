25809e4b9652905226b3230c1ecf7588e08a3749
ATOROYO <davidatoroyosika@gmail.com>
2024-10-26 18:41:42 +0300

    Debuged the server error

--- config/db.js
+++ config/db.js
@@ -3,15 +3,17 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 // Setting up database connection
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+const pool = mysql
+  .createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  })
+  .promise();
 
 (async () => {
   try {
@@ -23,4 +25,4 @@ const pool = mysql.createPool({
   }
 })();
 
-module.exports = pool.promise();
+module.exports = pool;
